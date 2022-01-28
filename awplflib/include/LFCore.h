1453dc9f4a564f5cd84ce21de6a4705dd91cbac0
NN-Videolab <telnykha@yahoo.com>
2022-01-28 22:01:32 +0300

    Add LFCore && LFInterface

---
+++ awplflib/include/LFCore.h
@@ -0,0 +1,253 @@
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+//  IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
+//
+//  By downloading, copying, installing or using the software you agree to this license.
+//  If you do not agree to this license, do not download, install,
+//  copy or use the software.
+//
+//
+//                           License Agreement
+//                  Locate Framework  Computer Vision Library
+//
+// Copyright (C) 2004-2018, NN-Videolab.net, all rights reserved.
+// Third party copyrights are property of their respective owners.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//   * Redistribution's of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//
+//   * Redistribution's in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other materials provided with the distribution.
+//
+//   * The name of the copyright holders may not be used to endorse or promote products
+//     derived from this software without specific prior written permission.
+//
+// This software is provided by the copyright holders and contributors "as is" and
+// any express or implied warranties, including, but not limited to, the implied
+// warranties of merchantability and fitness for a particular purpose are disclaimed.
+// In no event shall the NN-Videolab.net or contributors be liable for any direct,
+// indirect, incidental, special, exemplary, or consequential damages
+// (including, but not limited to, procurement of substitute goods or services;
+// loss of use, data, or profits; or business interruption) however caused
+// and on any theory of liability, whether in contract, strict liability,
+// or tort (including negligence or otherwise) arising in any way out of
+// the use of this software, even if advised of the possibility of such damage.
+//
+//      Locate Framework  (LF) Computer Vision Library.
+//		File: LFCore.h
+//		Purpose: Contains coomons for Locate Framework
+//
+//      CopyRight 2004-2022 (c) NN-Videolab.net
+//M*/
+
+#pragma once
+#ifndef  __lf_core_h__
+#define  __lf_core_h__
+
+#include "awpipl.h"
+
+/** \defgroup LFCommon
+*	Commnon classes for Locate Framework
+*   @{
+*/
+/** @brief Base class for all objects whithing library. Each object has the virtual destructor and name.
+*/
+class TLFObject
+{
+protected:
+public:
+	TLFObject();
+	virtual ~TLFObject();
+	virtual const char* GetName();
+};
+/** @brief List of LFObjects
+*/
+class TLFObjectList : public TLFObject
+{
+private:
+	TLFObject** m_List;
+	int              m_Count;
+	int              m_Capacity;
+protected:
+	virtual void Grow();
+	void Put(int index, TLFObject* pObject);
+public:
+	TLFObjectList();
+	~TLFObjectList();
+
+	int Add(TLFObject* pObject);
+	virtual void Clear();
+	void Delete(int index);
+
+	void Exchange(int index1, int index2);
+	TLFObjectList* Expand();
+	TLFObject* Extract(TLFObject* pObject);
+	TLFObject* Get(int index);
+
+	TLFObject* First();
+	int IndexOf(TLFObject* pObject);
+	void Insert(int index, TLFObject* pObject);
+	TLFObject* Last();
+	void Move(int nCurIndex, int nNewIndex);
+	int Remove(TLFObject* pObject);
+
+	void Pack();
+	void Sort(TLFListSortCompare Compare);
+
+	// property
+	int GetCapacity();
+	void SetCapacity(int NewCapacity);
+
+	int GetCount();
+	void SetCount(int NewCount);
+
+	TLFObject** GetList();
+	virtual const char* GetName()
+	{
+		return "TLFObjectList";
+	}
+};
+/** @brief Linked list node
+*/
+class TLFListNode : public TLFObject
+{
+private:
+	TLFListNode* m_next;
+	TLFObject* m_element;
+public:
+	TLFListNode(TLFObject* object);
+	TLFObject* GetElement();
+	void       SetElement(TLFObject* object);
+	TLFListNode* NextNode();
+	void		   SetNextNode(TLFListNode* node);
+
+	virtual const char* GetName()
+	{
+		return "TLFNode";
+	}
+};
+/** @brief Linked list
+*/
+class TLFList : TLFObject
+{
+	TLFListNode* m_headNode;
+public:
+	TLFList();
+	virtual ~TLFList();
+
+	TLFObject* First();
+	TLFObject* Last();
+	TLFObject* Next(TLFObject* object);
+	/*
+		полная очистка списка.
+	*/
+	void Clear(bool del = false);
+	void PushBack(TLFObject* object);
+	void PopBack();
+	TLFObject* Pop(TLFObject* object);
+
+	virtual const char* GetName()
+	{
+		return "TLFList";
+	}
+};
+/** @brief Raster data in the awpImage format
+*		   Supports Load, Save,
+*		   LoadDump, SaveDump
+*		   IntegralImage for each channel (for RGB 24 bit imiages)
+*/
+class  TLFImage : public TLFObject
+{
+protected:
+
+	double* m_lnpix;
+	double* m_sqpix;
+	double* m_rlnpix;
+	double* m_glnpix;
+	double* m_blnpix;
+
+	awpImage* m_pImage;					/*source image in the grayscale*/
+	awpImage* m_pIntegralImage;         /*integral image*/
+	awpImage* m_pIntegralSquareImage;   /*integral square image*/
+
+	awpImage* m_pRed;
+	awpImage* m_pGreen;
+	awpImage* m_pBlue;
+
+	awpImage* m_pIntegralRed;
+	awpImage* m_pIntegralGreen;
+	awpImage* m_pIntegralBlue;
+
+
+public:
+	TLFImage();
+	TLFImage(TLFImage& pImage);
+	virtual  ~TLFImage();
+
+	virtual bool LoadFromFile(const char* szFileName);
+	virtual bool SaveToFile(const char* szFileName);
+
+	// memory exchange
+	virtual bool SaveDump(const char* szFileName);
+	virtual bool LoadDump(const char* szFileName);
+	virtual bool LoadMemDump(const unsigned char* pDump, int nDumpSize);
+
+#ifdef WIN32
+	//	virtual bool LoadResourceImage(char* lpName);
+#endif
+
+	//properties
+	awpImage* GetImage();
+	awpImage* GetIntegralImage();
+	awpImage* GetSqIntegralImage();
+
+	awpImage* GetRedImage();
+	awpImage* GetGreenImage();
+	awpImage* GetBlueImage();
+
+	awpImage* GetRedIntegral();
+	awpImage* GetGreenIntegral();
+	awpImage* GetBlueIntegral();
+
+
+	virtual bool SetImage(awpImage* pImage);
+	bool         CopyImage(awpImage** ppImage);
+
+	double CalcLnSum(int x, int y, int w, int h);
+	double CalcSqSum(int x, int y, int w, int h);
+
+	double CalcRLnSum(int x, int y, int w, int h);
+	double CalcGLnSum(int x, int y, int w, int h);
+	double CalcBLnSum(int x, int y, int w, int h);
+
+	void FreeImages();
+
+#ifdef WIN32
+	void  Show(HDC dc, int x, int y, int w, int h, int from_x, int from_y);
+	void  StretchShow(HDC dc, int x, int y, int w, int h, int from_x, int from_y);
+#endif
+	virtual const char* GetName()
+	{
+		return "TLFImage";
+	}
+};
+/** @brief List TLFImage objects
+*/
+class TLFImageList : public TLFObjectList
+{
+public:
+	void AddImage(TLFImage* img);
+	/*	void AddImage(awpImage* img)
+	   {
+		   TLFImage* qq = new TLFImage();
+		   qq->SetImage(img);
+		   AddImage(qq);
+	   }*/
+	TLFImage* GetImage(int index);
+};
+#endif 
+/** @} */ /*  end of LFCommon group */
---
+++ awplflib/include/LFInterface.h
@@ -0,0 +1,282 @@
+#pragma once
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+//  IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
+//
+//  By downloading, copying, installing or using the software you agree to this license.
+//  If you do not agree to this license, do not download, install,
+//  copy or use the software.
+//
+//
+//                           License Agreement
+//                  Locate Framework  Computer Vision Library
+//
+// Copyright (C) 2004-2018, NN-Videolab.net, all rights reserved.
+// Third party copyrights are property of their respective owners.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//   * Redistribution's of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//
+//   * Redistribution's in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other materials provided with the distribution.
+//
+//   * The name of the copyright holders may not be used to endorse or promote products
+//     derived from this software without specific prior written permission.
+//
+// This software is provided by the copyright holders and contributors "as is" and
+// any express or implied warranties, including, but not limited to, the implied
+// warranties of merchantability and fitness for a particular purpose are disclaimed.
+// In no event shall the NN-Videolab.net or contributors be liable for any direct,
+// indirect, incidental, special, exemplary, or consequential damages
+// (including, but not limited to, procurement of substitute goods or services;
+// loss of use, data, or profits; or business interruption) however caused
+// and on any theory of liability, whether in contract, strict liability,
+// or tort (including negligence or otherwise) arising in any way out of
+// the use of this software, even if advised of the possibility of such damage.
+//
+//      Locate Framework  (LF) Computer Vision Library.
+//		File: LFinterface.h
+//		Purpose: Contains abstracts for Locate Framework
+//
+//      CopyRight 2004-2018 (c) NN-Videolab.net
+//M*/
+
+
+typedef struct _LFPoint {
+	double x;
+	double y;
+} LFPoint, *LPLFPoint;
+
+typedef struct _LFRect {
+	double x;
+	double y;
+} LFRect, * LPLFRect;
+
+class ILFImage
+{
+public:
+	virtual bool LoadFromFile(const char* fileName)   = 0;
+	virtual bool SaveToFileFile(const char* fileName) = 0;
+	// [todo:] possible we don't need unsigned char ????
+	virtual unsigned char* pixels(int channel) = 0;
+	virtual double* integral(int channel)  = 0;
+	virtual double* integral2(int channel) = 0;
+	virtual int width() = 0;
+	virtual int height() = 0;
+	virtual int channels() = 0;
+};
+
+typedef enum _LF_SHAPE_TYPE {
+	LFShapePoint = 0,
+	LFShapeVector,
+	LFShapeCircle,
+	LFShapeRect,
+	LFShapeEllipse, 
+	LFShapePolyline, 
+	LFShapePolygon, 
+	LFShapelandmarks,
+	LFShapeContext
+} LF_SHAPE_TYPE;
+
+typedef enum _LF_RACURS_TYPE {
+    LFRacursFront = 0,
+    LFRacursLeftSemiProfile,
+    LFRacursLeftProfile,
+	LFRacursLeftBackSemiProfile,
+	LFRacursBack,
+	LFRacursRightBackSemiProfile,
+	LFRacursRightProfile,
+	LFRacursRightSemProfile,
+	LFRacursBottomFront,
+	LFRacursBottomLeftSemiPropfile,
+	LFRacursBottomLeftProfile,
+	LFRacursBottomBackLeftSemiProfile,
+	LFRacursBottomBack,
+	LFRacursBottomBackRightSemiProfile,
+	LFRacursBottomRightProfile,
+	LFRacursBottomRightSemiPropfile,
+	LFRacursBottom,
+	LFRacursUpFront,
+	LFRacursUpLeftSemiProfile,
+	LFRacursUpLeftProfile,
+	LFRacursUpLeftBackSemiProfile,
+	LFRacursUpBack,
+	LFRacursUpBackRightSemiProfile,
+	LFRacursUpRightProfile,
+	LFRacursUpRightSemiProfile,
+	LFRacursUp
+} LF_RACURS_TYPE;
+
+// 
+class ILFWordAttribute
+{
+public:
+	virtual const char* attrLabel() = 0;
+	virtual const char* attrID()	= 0;
+	virtual double confidence()		= 0;
+};
+
+class ILFWordAttributes
+{
+public: 
+	virtual int Count() = 0;
+	virtual ILFWordAttribute* Attribute(int index) = 0;
+	virtual bool Delete(int index) = 0;
+	virtual bool Add(ILFWordAttribute* attribute) = 0;
+};
+
+//Word 
+class ILFWord
+{
+protected: 
+	LF_SHAPE_TYPE		m_shape;
+public:
+	virtual const char* classLabel() = 0;
+	virtual const char* classID() = 0;
+	virtual int classColor() = 0;
+	virtual ILFWordAttributes* Attributes() = 0;
+	LF_SHAPE_TYPE Shape();
+};
+
+class ILFDictinary
+{
+public: 
+	virtual const char* dictID() = 0;
+	virtual int Count() = 0;
+	virtual ILFWord* Word() = 0;
+	virtual bool Delete(int index) = 0;
+	virtual bool Add(ILFWord* word) = 0;
+};
+
+//item 
+class ILFItem
+{
+protected:
+	LF_RACURS_TYPE		m_racurs;
+	double				m_raiting;
+public:
+	virtual ILFWord* Word() = 0;
+	virtual int Count() = 0; 
+	virtual LFPoint Point(int index) = 0;
+};
+// descriptor 
+class ILFDescriptor
+{
+public:
+	virtual int Count() = 0;
+	virtual ILFItem*	Item(int index) = 0;
+	virtual ILFImage*   Foreground() = 0;
+
+	virtual bool LoadFromFile(const char* fileName) = 0;
+	virtual bool SaveToFile(const char* fileName)   = 0;
+};
+
+// Zone
+class ILFZone
+{
+public: 
+	virtual int			Count() = 0;
+	virtual LPLFPoint	Point(int index) = 0;
+	virtual bool		Delete(int index) = 0;
+	virtual bool		Add(LFPoint& point) = 0;
+};
+// Zones 
+class ILFZones
+{
+public:
+	virtual int Count() = 0;
+	virtual ILFZone* Zone(int index) = 0;
+	virtual bool Delete(int index)   = 0;
+	virtual bool Add(ILFZone* zone)  = 0;
+};
+// feature
+class _ILFFeature
+{
+public: 
+	virtual void shift(LFPoint delta)		  = 0;
+	virtual void scale(double factor)		  = 0;
+	virtual void setup(double factor, LFPoint shift) = 0;
+	virtual unsigned int uValue()			  = 0;
+	virtual double dValue()					  = 0;
+	virtual LFPoint corner()				  = 0;
+	virtual int width()						  = 0;
+	virtual int height()					  = 0;
+};
+// weak 
+class _ILFWeak
+{
+public:
+	virtual _ILFFeature* feature() = 0;
+	virtual void setup(double factor, LFPoint shift) = 0;
+	virtual double wight() = 0;
+	virtual void setWeight(const double value) = 0;
+	virtual bool classify(ILFImage* image, double& value) = 0;
+};
+// strong 
+class _ILFStrong
+{
+public: 
+	virtual int Count() = 0;
+	virtual _ILFWeak* Weak(int index) = 0;
+	virtual void AddWeak(_ILFWeak* weak) = 0;
+	virtual double threshold() = 0;
+	virtual double setThreshold(const double value) = 0;
+	virtual void setup(double factor, LFPoint shift) = 0;
+	virtual int classify(ILFImage* image, double& value) = 0;
+};
+// cascade 
+class _ILFCascade
+{
+public: 
+	virtual int Count()														= 0;
+	virtual _ILFStrong* Strong(int index)									= 0;
+	virtual int classify(ILFImage* image, LFRect& rect, double& value)		= 0;
+	virtual void parallel(ILFImage* image, LFRect& rect, double* weights)   = 0;
+	virtual bool AddStrong(_ILFStrong* strong)								= 0;
+	virtual bool DeleteStrong(int index)									= 0;
+};
+// scanner 
+// 
+// attribute detector 
+// detector 
+class _ILFTrainer;
+class _ILFDetector
+{
+public: 
+	virtual int Count() = 0;
+	virtual _ILFCascade* Cascade(int index) = 0;
+	virtual bool Delete(int index)		    = 0;
+	virtual bool Add(_ILFCascade* cascade)  = 0;
+	virtual bool Detect(ILFImage* image)    = 0;
+	virtual ILFDescriptor* Descriptor()     = 0;
+	virtual _ILFTrainer* Trainer()			= 0;
+};
+ 
+//Model
+class ILFEngine;
+class ILFModel
+{
+public:
+	virtual ILFDictinary* Dictinary() = 0;
+	virtual ILFEngine* Engine() = 0;
+};
+
+// Engine 
+class ILFEngine
+{
+public: 
+	virtual bool Init(const char* lpModelName) = 0;
+	virtual bool Forward(ILFImage* image) = 0;
+	virtual bool Update(const char* path) = 0;
+	virtual ILFDescriptor* Descriptor() = 0;
+	virtual ILFModel* Model() = 0;
+
+	/*surveillance zones*/
+	virtual ILFZones* Zones() = 0;
+	
+};
+// database 
