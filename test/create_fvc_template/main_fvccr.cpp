c3b19bd901c368b59312f1cc95fdd7db64be1492
olgashemagina <olgashemagina@gmail.com>
2022-03-28 20:21:35 +0300

    GetPCAFeatures(part) added to Test solution

--- test/create_fvc_template/main_fvccr.cpp
+++ test/create_fvc_template/main_fvccr.cpp
@@ -7,7 +7,7 @@
 
 #include <stdio.h>
 
-#include <io.h>
+//#include <io.h>
 #include <string>
 
 #include "_LF.h"
@@ -19,7 +19,7 @@ void Usage()
 	printf("Usage: fvccreate fvlcreate.xml\n");
 }
 
- int _tmain(int argc, _TCHAR* argv[]) 
+ int main(int argc, char* argv[]) 
 {
 	if (argc < 2)
 	{
@@ -29,7 +29,7 @@ void Usage()
 
 	// main program
 	TiXmlDocument doc;
-	if (!doc.LoadFile((const char*)argv[1]))
+	if (!doc.LoadFile(argv[1]))
 	{
 		printf("error: cannot load configuration.\n");
 		return 1;
@@ -55,6 +55,7 @@ void Usage()
 	int width  = 24;
 	int height = 24;
 	int num_vct = 16;
+	string format = pElem->Attribute("format");
 	pElem->QueryIntAttribute("width",  &width);
 	pElem->QueryIntAttribute("height",  &height);
 	pElem->QueryIntAttribute("num_vct",  &num_vct);
@@ -63,6 +64,7 @@ void Usage()
 
 	printf("input database: %s\n", path.c_str());
 	printf("out name: %s\n", outName.c_str());
+	printf("images format: %s\n", format.c_str());
 	printf("width: %i\n", width);
 	printf("height: %i\n", height);
 	printf("num vectors: %i\n", num_vct);
@@ -70,11 +72,11 @@ void Usage()
    FvcTemplate* pTemplate = NULL;
 
 	//string path( dirName );
-	string path1 = path + "\\*.awp";
+   string path1 = path + "\\*." + format;
 	_finddata_t filesInfo;
 	int  numFiles = 0;
-	long handle = 0;
-	if ( (handle = _findfirst( (char*)((path+"*.awp").c_str()), &filesInfo)) != -1 )
+	intptr_t  handle = 0;
+	if ( (handle = _findfirst( (char*)(path1.c_str()), &filesInfo)) != -1 )
 	{
 		do
 		{
@@ -91,20 +93,22 @@ void Usage()
 	}
 	int numVect = num_vct;
 	awpImage** sourceImages = new awpImage* [numFiles];
-	handle = _findfirst((char*)((path+"*.awp").c_str()), &filesInfo );
+	handle = _findfirst((char*)(path1.c_str()), &filesInfo );
 	string fileName;
 
 
 	for( int i=0; i<numFiles; i++  ){
-		//cout << filesInfo.name << endl;
+		cout << filesInfo.name << endl;
 		cout << ".";
-		fileName = path + filesInfo.name;
+		fileName = path + "\\" + filesInfo.name;
 
 		AWPRESULT load_res = awpLoadImage( fileName.c_str(), &sourceImages[i] );
-	   //	awpConvert(sourceImages[i], AWP_CONVERT_TO_FLOAT);
+	   	awpConvert(sourceImages[i], AWP_CONVERT_TO_FLOAT);
+		cout << fileName << endl;
 		if( load_res )
 			cout << load_res;
-		if( sourceImages[i]->sSizeX != width && sourceImages[i]->sSizeY != height){
+		if( sourceImages[i]->sSizeX != width && sourceImages[i]->sSizeY != height)
+		{
 			AWPRESULT resize_res = awpResize( sourceImages[i], width, height );
 			cout << resize_res;
 		}
